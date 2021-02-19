5365fc4d4e26e8f79a41d47838c0a31c3ab409e8
chxiaoyuW10 <961041421@qq.com>
2021-02-19 11:14:56 +0800

    完成首页商品列表内容

---
+++ components/w-goods-item/w-goods-item.js
@@ -0,0 +1,29 @@
+Component({
+  behaviors: [],
+  properties: {
+    item: {
+      type: Object,
+      value: {}
+    }
+  },
+  data: {
+
+  },
+  lifetimes: {
+    created() {
+
+    },
+    attached() {
+
+    },
+    moved() {
+
+    },
+    detached() {
+
+    },
+  },
+  methods: {
+
+  },
+});
---
+++ components/w-goods/w-goods.js
@@ -0,0 +1,29 @@
+Component({
+  behaviors: [],
+  properties: {
+    goods: {
+      type: Array,
+      value: []
+    }
+  },
+  data: {
+
+  },
+  lifetimes: {
+    created() {
+
+    },
+    attached() {
+
+    },
+    moved() {
+
+    },
+    detached() {
+
+    },
+  },
+  methods: {
+
+  },
+});
